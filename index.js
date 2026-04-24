7d6b11071a54181188fc5c79e8b3feec895720c0
jerryalpaca007 <ajayjadenp@gmail.com>
2026-04-24 13:39:58 +0530

    you can now see images of pokemon

---
+++ index.js
@@ -0,0 +1,30 @@
+async function fetchPokemon() {
+  const input = document.getElementById("txtbox").value.toLowerCase();
+  const img = document.getElementById("pokemonImage");
+
+  const url = `https://pokeapi.co/api/v2/pokemon/${input}`;
+
+  try {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error("Pokemon not found");
+    }
+
+    const data = await response.json();
+    const imageUrl = data.sprites.other["official-artwork"].front_default;
+    const attack1n = data.abilities["ability"].name;
+    const attack1url = data.abilities["ability"].url;
+
+    img.src = imageUrl;
+    img.style.display = "block";
+  } catch (error) {
+    alert("Pokémon not found 😢");
+    img.style.display = "none";
+  }
+}
+document.getElementById("txtbox").addEventListener("keypress", function (e) {
+  if (e.key === "Enter") {
+    fetchPokemon();
+  }
+});
